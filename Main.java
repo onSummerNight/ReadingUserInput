1df9ea34032c196834dd2e0b3bb13a5b66b1a6d0
isSpringNight <mail.hamdinugraha@gmail.com>
2023-10-17 18:46:50 +0700

    add read user input

---
+++ Main.java
@@ -0,0 +1,28 @@
+public class Main {
+    public static void main(String[] args) {
+        int currentYear = 2022;
+//        String userDateOfBirth = "1999";
+//
+//        int dateOfBirth = Integer.parseInt(userDateOfBirth);
+//
+//        System.out.println("Age = " + (currentYear - dateOfBirth));
+//
+//        String userAgeWithPartialyear = "22.5";
+//        double ageWithPartialYear = Double.parseDouble(userAgeWithPartialyear);
+//        System.out.println("The user says he's " + ageWithPartialYear);
+
+        System.out.println(getInputFromConsole(currentYear));
+    }
+
+    public static String getInputFromConsole(int currentYear){
+
+        return "";
+    }
+
+    public static String getInputFromScanner(int currentYear){
+
+        return "";
+    }
+
+
+}

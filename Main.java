4e375a81fc388ee952b31976562dd28f73c1126f
BuildTools <unconfigured@null.spigotmc.org>
2023-10-17 22:58:37 +0700

    updating main class

--- Main.java
+++ Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         int currentYear = 2023;
@@ -10,9 +12,12 @@ public class Main {
 //        String userAgeWithPartialyear = "22.5";
 //        double ageWithPartialYear = Double.parseDouble(userAgeWithPartialyear);
 //        System.out.println("The user says he's " + ageWithPartialYear);
+        try {
+            System.out.println(getInputFromConsole(currentYear));
+        } catch (NullPointerException e){
+            System.out.println(getInputFromScanner(currentYear));
+        }
 
-        System.out.println(getInputFromConsole(currentYear));
-        System.out.println(getInputFromScanner(currentYear));
     }
 
     public static String getInputFromConsole(int currentYear){
@@ -28,7 +33,18 @@ public class Main {
 
     public static String getInputFromScanner(int currentYear){
 
-        return "";
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Hi, What's your name? ");
+        String name = scanner.nextLine();
+
+        System.out.println("Hi " + name + ", Welcome to the jungle");
+
+        System.out.println("What year were you born? ");
+        String dateOfBirth = scanner.nextLine();
+        int age = currentYear - Integer.parseInt(dateOfBirth);
+
+        return "So you are " + age + " years old";
     }
 
 
